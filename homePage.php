3ab9a101d56b2c8acbbec0437b7f893ee807ef76
skukudov <stanimir.kukudov@netea.bg>
2015-12-20 22:08:14 +0200

    login fb

--- homePage.php
+++ homePage.php
@@ -1,3 +1,49 @@
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '1710186109195271',
+			xfbml      : true,
+			version    : 'v2.5',
+			cookie     : true
+		});
+	};
+
+	// Load the SDK asynchronously
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	function fb_login(){
+		FB.login(function(response) {
+
+			if (response.authResponse) {
+				console.log('Welcome!  Fetching your information.... ');
+				//console.log(response); // dump complete info
+				access_token = response.authResponse.accessToken; //get access token
+				user_id = response.authResponse.userID; //get FB UID
+
+				FB.api('/me', function(response) {
+					var names = response.name.split(" ");
+					var lastNameParam = (names.length > 1) ? "&lname=" + names[1] : "";
+					var emailParam = (typeof response.email !== "undefined") ? "&email=" + response.email : "";
+					window.location.href = "/register?fname=" + names[0] + lastNameParam + emailParam;
+				});
+
+			} else {
+				//user hit cancel button
+				console.log('User cancelled login or did not fully authorize.');
+
+			}
+		}, {
+			scope: 'public_profile,email'
+		});
+	}
+</script>
+
 <div id="homepage" class="center-block text-center">
 	<div id="homepage-text">
 		Lorem Ipsum è un testo segnaposto<br />
@@ -7,5 +53,5 @@
 		standard sin dal sedicesimo secolo,<br />
 		quando un anonimo tipografo.
 	</div>
-	<a href="/register" id="homepage-button"></a>
+	<a href="javascript:void(0)" onclick="fb_login();" id="homepage-button"></a>
 </div>
