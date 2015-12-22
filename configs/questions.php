0d92ea564527837a1d9c3a2e658ffd638509d810
skukudov <stanimir.kukudov@netea.bg>
2015-12-22 23:22:13 +0200

    questions

---
+++ configs/questions.php
@@ -0,0 +1,68 @@
+<?php
+
+$questions = array(
+    'question' => array(
+        1 => 'Se un/a tuo/a amico/a è triste, cosa faresti per cercare di tirarlo/a su di morale?',
+        2 => 'Con cosa preferiresti accompagnare la visione di un film?',
+        3 => 'Dove passeresti l\'ultimo dell\'anno?',
+        4 => 'Con quale mezzo di trasporto partiresti per un viaggio in Nord Europa?',
+        5 => 'Quando sei in viaggio, preferisci fare colazione come faresti a casa o ti adatti alle usanze locali?',
+        6 => 'Scopri che la tua band preferita, la sera stessa, suona in un locale della tua città. Cosa faresti?',
+        7 => 'Quando ripensi alla tua infanzia, a cosa è legato il primo ricordo che ti viene in mente?',
+        8 => 'Quale cantante preferisci tra:',
+        9 => 'Qual è il tuo personaggio storico preferito tra:',
+        10 => 'Se dovessi vincere un ricco premio in denaro, qual è la prima cosa che faresti?'
+    ),
+    'answers' => array(
+        1 => array(
+            array('question' => 'Proporrei di andare a fare una passeggiata al mare o in montagna.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Cercherei di parlare con lui/lei per capire come affrontare la situazione.', 'profile' => 'SAGGIO'),
+            array('question' => 'Cercherei di accontentare qualsiasi sua richiesta per passare al meglio la giornata insieme.', 'profile' => 'ROMANTICO')
+        ),
+        2 => array(
+            array('question' => 'Con un dolce.', 'profile' => 'ROMANTICO'),
+            array('question' => 'Con delle patatine.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Con un tè o un caffè.', 'profile' => 'SAGGIO')
+        ),
+        3 => array(
+            array('question' => 'In un luogo al chiuso. (Es.: ristorante, discoteca, ecc.)', 'profile' => 'SAGGIO'),
+            array('question' => 'In una festa in piazza.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Non importa dove, basta che ci sia la mia dolce metà.', 'profile' => 'ROMANTICO')
+        ),
+        4 => array(
+            array('question' => 'In auto o in moto.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'In treno.', 'profile' => 'ROMANTICO'),
+            array('question' => 'In aereo.', 'profile' => 'SAGGIO')
+        ),
+        5 => array(
+            array('question' => 'Cerco di adattarmi.', 'profile' => 'SAGGIO'),
+            array('question' => 'Voglio il mio caffè!', 'profile' => 'ROMANTICO'),
+            array('question' => 'Assaggerei di tutto.', 'profile' => 'AVVENTURIERO')
+        ),
+        6 => array(
+            array('question' => 'Chiederei a qualcuno se viene con me.', 'profile' => 'ROMANTICO'),
+            array('question' => 'Ci andrei anche da solo/a.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Acquisterei due biglietti e ci porterei il mio migliore amico/migliore amica.', 'profile' => 'SAGGIO')
+        ),
+        7 => array(
+            array('question' => 'Alla mia famiglia.', 'profile' => 'SAGGIO'),
+            array('question' => 'Ai mie amici.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Alla mia “fidanzatina”.', 'profile' => 'ROMANTICO')
+        ),
+        8 => array(
+            array('question' => 'Vasco Rossi', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Tiziano Ferro', 'profile' => 'ROMANTICO'),
+            array('question' => 'Franco Battiato', 'profile' => 'SAGGIO')
+        ),
+        9 => array(
+            array('question' => 'Madre Teresa di Calcutta', 'profile' => 'ROMANTICO'),
+            array('question' => 'Mahatma Gandhi', 'profile' => 'SAGGIO'),
+            array('question' => 'Cristoforo Colombo', 'profile' => 'AVVENTURIERO')
+        ),
+        10 => array(
+            array('question' => 'Partirei in un viaggio in giro per il mondo.', 'profile' => 'AVVENTURIERO'),
+            array('question' => 'Comprerei casa.', 'profile' => 'SAGGIO'),
+            array('question' => 'Regalerei una parte della vincita alla mia famiglia.', 'profile' => 'ROMANTICO')
+        )
+    )
+);
--- quiz.php
+++ quiz.php
@@ -1,3 +1,20 @@
-<?php
+<script src="/js/spin.js"></script>
+<script src="/js/jquery.form.js"></script>
+<script src="/js/CacheLocal.js"></script>
 
-echo 'quiz page goes here!';
+<div id="questions" class="center-block text-center">
+    <div id="question" class="text-uppercase"></div>
+    <form method="POST" action="">
+        <div class="form-group">
+            <button id="question-button-1"><span class="questions-text"></span></button>
+            <button id="question-button-2"><span class="questions-text"></span></button>
+            <button id="question-button-3"><span class="questions-text"></span></button>
+        </div>
+    </form>
+</div>
+
+<script>
+    $(document).ready(function () {
+
+    });
+</script>
