2b0742891c92d3bbf5b4e5958c797ba15587e416
skukudov <stanimir.kukudov@netea.bg>
2015-12-23 16:32:52 +0200

    final state

--- register.php
+++ register.php
@@ -16,11 +16,123 @@ if ($sessUser->getIsLoggedIn()) {
 
 $provinces = array(
     '0' => 'SELEZIONA PROVINCIA',
-    'italia' => 'Italia'
+    'agrigento' => 'Agrigento',
+    'alessandria' => 'Alessandria',
+    'ancona' => 'Ancona',
+    'aosta' => 'Aosta',
+    'arezzo' => 'Arezzo',
+    'ascoli_piceno' => 'Ascoli Piceno',
+    'asti' => 'Asti',
+    'avellino' => 'Avellino',
+    'bari' => 'Bari',
+    'barletta_andria_trani' => 'Barletta-Andria-Trani',
+    'belluno' => 'Belluno',
+    'benevento' => 'Benevento',
+    'bergamo' => 'Bergamo',
+    'biella' => 'Biella',
+    'bologna' => 'Bologna',
+    'bolzano' => 'Bolzano',
+    'brescia' => 'Brescia',
+    'brindisi' => 'Brindisi',
+    'cagliari' => 'Cagliari',
+    'caltanissetta' => 'Caltanissetta',
+    'campobasso' => 'Campobasso',
+    'carbonia_iglesias' => 'Carbonia-Iglesias',
+    'caserta' => 'Caserta',
+    'catania' => 'Catania',
+    'catanzaro' => 'Catanzaro',
+    'chieti' => 'Chieti',
+    'como' => 'Como',
+    'cosenza' => 'Cosenza',
+    'cremona' => 'Cremona',
+    'crotone' => 'Crotone',
+    'cuneo' => 'Cuneo',
+    'enna' => 'Enna',
+    'ferrara' => 'Ferrara',
+    'firenze' => 'Firenze',
+    'foggia' => 'Foggia',
+    'forlì_cesena' => 'Forlì-Cesena',
+    'frosinone' => 'Frosinone',
+    'genova' => 'Genova',
+    'gorizia' => 'Gorizia',
+    'grosseto' => 'Grosseto',
+    'imperia' => 'Imperia',
+    'isernia' => 'Isernia',
+    'l_aquila' => 'L\'Aquila',
+    'la_spezia' => 'La Spezia',
+    'latina' => 'Latina',
+    'lecce' => 'Lecce',
+    'lecco' => 'Lecco',
+    'livorno' => 'Livorno',
+    'lodi' => 'Lodi',
+    'lucca' => 'Lucca',
+    'macerata' => 'Macerata',
+    'mantova' => 'Mantova',
+    'massa_carrara' => 'Massa-Carrara',
+    'matera' => 'Matera',
+    'medio_campidano' => 'Medio Campidano',
+    'messina' => 'Messina',
+    'milano' => 'Milano',
+    'modena' => 'Modena',
+    'monza_brianza' => 'Monza-Brianza',
+    'napoli' => 'Napoli',
+    'novara' => 'Novara',
+    'nuoro' => 'Nuoro',
+    'ogliastra' => 'Ogliastra',
+    'olbia_tempio' => 'Olbia-Tempio',
+    'oristano' => 'Oristano',
+    'padova' => 'Padova',
+    'palermo' => 'Palermo',
+    'parma' => 'Parma',
+    'pavia' => 'Pavia',
+    'perugia' => 'Perugia',
+    'pesaro_e_urbino' => 'Pesaro e Urbino',
+    'pescara' => 'Pescara',
+    'piacenza' => 'Piacenza',
+    'pisa' => 'Pisa',
+    'pistoia' => 'Pistoia',
+    'pordenone' => 'Pordenone',
+    'potenza' => 'Potenza',
+    'prato' => 'Prato',
+    'ragusa' => 'Ragusa',
+    'ravenna' => 'Ravenna',
+    'reggio_calabria' => 'Reggio Calabria',
+    'reggio_emilia' => 'Reggio Emilia',
+    'rieti' => 'Rieti',
+    'rimini' => 'Rimini',
+    'roma' => 'Roma',
+    'rovigo' => 'Rovigo',
+    'salerno' => 'Salerno',
+    'sassari' => 'Sassari',
+    'savona' => 'Savona',
+    'siena' => 'Siena',
+    'siracusa' => 'Siracusa',
+    'sondrio' => 'Sondrio',
+    'taranto' => 'Taranto',
+    'teramo' => 'Teramo',
+    'terni' => 'Terni',
+    'torino' => 'Torino',
+    'trapani' => 'Trapani',
+    'trento' => 'Trento',
+    'treviso' => 'Treviso',
+    'trieste' => 'Trieste',
+    'udine' => 'Udine',
+    'varese' => 'Varese',
+    'venezia' => 'Venezia',
+    'verbano_cusio_ossola' => 'Verbano-Cusio-Ossola',
+    'vercelli' => 'Vercelli',
+    'verona' => 'Verona',
+    'vibo_valentia' => 'Vibo Valentia',
+    'vicenza' => 'Vicenza',
+    'viterbo' => 'Viterbo'
 );
 $professions = array(
-    '0' => 'PROFESSIONE',
-    'developer' => 'Developer'
+    '0' => 'SELEZIONA PROFESSIONE',
+    'pubblico' => 'Pubblico',
+    'statale' => 'Statale',
+    'pensionato' => 'Pensionato',
+    'privato' => 'Privato',
+    'altro' => 'Altro'
 );
 
 $fbId = '';
@@ -99,7 +211,8 @@ if (!empty($_POST)) {
             'province' => $province,
             'phone' => $phone,
             'profession' => $profession
-        ))) {
+        ))
+        ) {
             header("Location: login?fb_id=" . $fbId);
             exit();
         }
@@ -115,30 +228,36 @@ if (!empty($_POST)) {
     <div id="register-form">
         <form action="/register" method="POST">
             <div class="form-group <?php echo (!empty($errors['fName'])) ? 'has-error' : ''; ?>">
-                <input type="text" class="form-control" name="fname" required placeholder="NOME" value="<?php echo $fName ?>">
+                <input type="text" class="form-control" name="fname" required placeholder="NOME"
+                       value="<?php echo $fName ?>">
             </div>
             <div class="form-group <?php echo (!empty($errors['lName'])) ? 'has-error' : ''; ?>">
-                <input type="text" class="form-control" name="lname" required placeholder="COGNAME" value="<?php echo $lName ?>">
+                <input type="text" class="form-control" name="lname" required placeholder="COGNAME"
+                       value="<?php echo $lName ?>">
             </div>
             <div class="form-group <?php echo (!empty($errors['province'])) ? 'has-error' : ''; ?>">
                 <select name="province" class="form-control">
                     <?php foreach ($provinces as $provinceKey => $provinceValue): ?>
-                        <option value="<?php echo $provinceKey; ?>" <?php echo ($province == $provinceKey) ? 'checked="checked"' : ''; ?>>
+                        <option
+                            value="<?php echo $provinceKey; ?>" <?php echo ($province == $provinceKey) ? 'checked="checked"' : ''; ?>>
                             <?php echo $provinceValue; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
             </div>
             <div class="form-group <?php echo (!empty($errors['phone'])) ? 'has-error' : ''; ?>">
-                <input type="number" class="form-control" required name="phone" placeholder="TELEFONO" value="<?php echo $phone ?>">
+                <input type="number" class="form-control" required name="phone" placeholder="TELEFONO"
+                       value="<?php echo $phone ?>">
             </div>
             <div class="form-group <?php echo (!empty($errors['email'])) ? 'has-error' : ''; ?>">
-                <input type="email" class="form-control" required name="email" placeholder="EMAIL" value="<?php echo $email ?>">
+                <input type="email" class="form-control" required name="email" placeholder="EMAIL"
+                       value="<?php echo $email ?>">
             </div>
             <div class="form-group <?php echo (!empty($errors['profession'])) ? 'has-error' : ''; ?>">
                 <select name="profession" class="form-control">
                     <?php foreach ($professions as $professionKey => $professionValue): ?>
-                        <option value="<?php echo $professionKey; ?>" <?php echo ($profession == $professionKey) ? 'checked="checked"' : ''; ?>>
+                        <option
+                            value="<?php echo $professionKey; ?>" <?php echo ($profession == $professionKey) ? 'checked="checked"' : ''; ?>>
                             <?php echo $professionValue; ?>
                         </option>
                     <?php endforeach; ?>
